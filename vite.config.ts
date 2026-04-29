c31320234578e92afa8c62984b65d1082ef77998
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2026-04-29 22:48:16 +0300

    feat(env): добавлен файл .example.env и добавлен .env в .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [".ngrok-free.app"],
     proxy: {
       "/api": {
         target: "http://localhost:3000",
